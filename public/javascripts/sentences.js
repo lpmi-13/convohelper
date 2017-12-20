b2cd8db8b5adea9e28d5a79c018cc2cef7d1ffd3
lpmi-13 <leskis@gmail.com>
2017-12-20 22:32:25 +0000

    remove console.log

--- public/javascripts/sentences.js
+++ public/javascripts/sentences.js
@@ -1,13 +1,13 @@
-var sentences = {};
-
-sentences.agree = ['I definitely agree', 'I think so too', 'Yeah, I agree', 'I can see your point', 'uh huh', 'Absolutely, I totally agree', 'That\'s a very good point', 'Yes, that\'s true'];
-
-sentences.disagree = ['I\'m not sure I agree', 'I don\'t really think so', 'well, I don\'t know about that', 'I take your point, but...', 'I understand what you\'re saying, but...', 'I agree with you up to a point, but...', 'That\'s not necessarily true...'];
-
-sentences.confirmation = ['So what you\'re saying is...', 'So in your view...', 'If I understand what you\'re saying...', 'So what you mean is...'];
-
-sentences.clarification = ['I\'m not sure I understand what you mean.','I didn\'t quite follow you.', 'Could you explain that point again, please?', 'Could you repeat that, plese?', 'Could you give an example of that, please?'];
-
-sentences.reviewing = ['Coming back to what I said earlier...', 'To the point from earlier...', 'To go back to my earlier point...'];
-
-sentences.requestFloor = ['Could I just make a quick point?', 'Ummmm...', 'I uh...', 'I ummm...'];
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
--- routes/messages.js
+++ routes/messages.js
@@ -1,41 +1,41 @@
-var express = require('express');
-var router = express.Router();
-var Pusher = require('pusher');
-//var configVars = require('./envVars');
-var give = require('./giveFloor');
-
-var app_id = process.env.APP_ID || configVars.app_id;
-var app_key = process.env.APP_KEY || configVars.app_key;
-var app_secret = process.env.APP_SECRET || configVars.app_secret;
-
-var pusher = new Pusher({
-  appId: app_id,
-  key: app_key,
-  secret: app_secret,
-  cluster: 'eu',
-  encrypted: true
-});
-
-router.get('/', function(req, res, next) {
-  var room = req.query.room;
-  console.log(room);
-  console.log('signing into room: ' + room);
-  res.render('channel', { title : 'ConvoHelper'});
-});
-
-//respond to incoming pusher events
-router.post('/', function(req, res, next) {
-  var socketID = req.body.socketId;
-  console.log('message from ' + socketID);
-
-  var number = Math.floor(Math.random() * give.sentences.length);
-  console.log(number);
-  var givingFloor = give.sentences[number];
-  console.log(givingFloor);
-
-  var room = req.body.room;
-  console.log('sending ' + givingFloor);
-  pusher.trigger(room, 'my_event', {'message': givingFloor}, socketID);
-});
-
-module.exports = router;
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
+onsole.log/d
